805f5d3d50521eefea72997f0cf439de15fdf550
MrKing80 <mr.king80729@gmail.com>
2025-04-23 10:44:29 +0900

    エネミーデータスクリプトを作成

---
+++ Assets/SASAKI/Scripts/EnemyData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class EnemyData
+{
+    [Header("敵のステータス")]
+    [SerializeField, Header("名前")] private string _enemyName = "";
+    [SerializeField, Header("ヒットポイント")] private int _enemyHp = 0;
+    [SerializeField, Header("攻撃力")] private int _attackPower = 0;
+    [SerializeField, Header("攻撃速度")] private int _attackSpeed = 0;
+    [SerializeField, Header("移動速度")] private int _moveSpeed = 0;
+}
