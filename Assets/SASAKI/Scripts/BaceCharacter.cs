065d6230fc710c75eb31ab6f34b38ae2043d30cb
MrKing80 <mr.king80729@gmail.com>
2025-05-14 15:46:46 +0900

    キャラクターベースクラス作成

---
+++ Assets/SASAKI/Scripts/BaceCharacter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// キャラクターのベースクラス
+/// 作成者：佐々木
+/// </summary>
+public class BaceCharacter
+{
+    protected string _characterName = "";
+    protected int _hp = 0;
+    protected float _attackPower = 0f;
+    protected float _attackSpeed = 0f;
+    protected float _moveSpeed = 0f;
+    //後で属性の情報を格納する変数を書き足す
+
+    public BaceCharacter()
+    {
+
+    }
+}
