d9f1b5817493cc18e7c93fcc2ffb2ef3e68f2039
MrKing80 <mr.king80729@gmail.com>
2025-05-30 13:32:47 +0900

    テストマップにいろいろ配置

---
+++ Assets/TAMAI/Scripts/MagicAttack/ShiningAttack.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+/// <summary>
+/// 光属性（初期属性）の攻撃クラス
+/// 作成者：佐々木
+/// </summary>
+[CreateAssetMenu(menuName = "Skills/ShiningAttack")]
+public class ShiningAttack : BaseAttribute
+{
+
+    public ShiningAttack()
+    {
+        //ここにこのスクリプトの初期化処理を書こう
+    }
+
+    public override void AttributeCharacteristics()
+    {
+        //TODO : 自然回復する処理を書く
+    }
+
+}
